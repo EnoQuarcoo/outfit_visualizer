af63678d8807f3d13da07202f8bb9909120384b4
EnoQuarcoo <emq06642@uga.edu>
2026-04-01 20:38:34 -0400

    Updated file structure for back and frontend

---
+++ frontend/src/components/CTASection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CTASection = () => {
+  return (
+    <div> <p> Drop your email below </p>
+    <input type="text" value={value} />
+    </div>
+  )
+}
+
+export default CTASection
